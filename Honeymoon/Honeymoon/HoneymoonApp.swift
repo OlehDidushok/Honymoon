2cbb085ce591884e843b0017acb127c5b7a35b0c
Oleh Didushok <didushokov@gmail.com>
2024-11-24 21:07:39 +0200

    Added images, sounds, icons

---
+++ Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Oleh on 24.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
