ea5fde6317c33db1e82761f51ec1724aac1897ec
Oleh Didushok <didushokov@gmail.com>
2024-11-27 16:13:39 +0200

    Implemented the card deck with a computed property

--- Honeymoon/Honeymoon/View/ContentView.swift
+++ Honeymoon/Honeymoon/View/ContentView.swift
@@ -13,14 +13,41 @@ struct ContentView: View {
     @State var showGuide: Bool = false
     @State var showInfo: Bool = false
     
+    // MARK: - CARDS VIEWS
+    var cardViews: [CardView] = {
+        var views = [CardView]()
+        
+        for index in 0..<2 {
+            views.append(CardView(honeymoon: honeymoonData[index]))
+        }
+        return views
+    }()
+    
+    // MARK: TOP CARD
+    private func isTopCard(cardView: CardView) -> Bool {
+        guard let index = cardViews.firstIndex(where: { $0.id == cardView.id
+        }) else { return false }
+        return index == 0
+    }
+    
     var body: some View {
         VStack {
+            // HEADER
             HeaderView(showGuide: $showGuide, showInfoView: $showInfo)
             Spacer()
-            CardView(honeymoon: honeymoonData[1])
-                .padding()
+            
+            // CARDS
+            ZStack {
+                ForEach(cardViews) { cardView in
+                    cardView
+                        .zIndex(isTopCard(cardView: cardView) ? 1 : 0)
+                }
+            }
+            .padding(.horizontal)
+            
             Spacer()
             
+            // FOOTER
             FooterView(showBookingAlert: $showAlert)
         }
         .alert(isPresented: $showAlert) {
